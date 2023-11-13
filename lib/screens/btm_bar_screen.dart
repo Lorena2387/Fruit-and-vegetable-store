4a5fb22f033931c5d814deef3601ec9922668eab
Lorena2387 <lorenaramirez23@hotmail.com>
2023-11-13 14:04:57 -0500

    create the pages of the navigator bar button

--- lib/screens/btm_bar_screen.dart
+++ lib/screens/btm_bar_screen.dart
@@ -1,5 +1,9 @@
+import 'package:app_frutas_verduras/screens/cart.dart';
+import 'package:app_frutas_verduras/screens/categories.dart';
 import 'package:app_frutas_verduras/screens/home_screen.dart';
+import 'package:app_frutas_verduras/screens/user.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
 class BottomBarScreen extends StatefulWidget {
   const BottomBarScreen({super.key});
@@ -9,9 +13,32 @@ class BottomBarScreen extends StatefulWidget {
 }
 
 class _BottomBarScreenState extends State<BottomBarScreen> {
-  List _pages = [HomeScreen()];
+  int _selectedIndex = 0;
+  final List _pages = [
+    const HomeScreen(),
+    const CategoriesScreen(),
+    const CartScreen(),
+    const UserScreen(),
+  ];
+  void _selectedPages(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _selectedPages,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(IconlyLight.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(IconlyLight.category), label: 'categories'),
+          ]),
+    );
   }
 }
