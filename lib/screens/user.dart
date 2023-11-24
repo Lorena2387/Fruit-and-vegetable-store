d34fbf0077f13bfe384b16bf50380b3069450d83
Lorena2387 <lorenaramirez23@hotmail.com>
2023-11-23 21:22:47 -0500

    implement the different items of the user profile configuration

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,30 +1,66 @@
+import 'package:app_frutas_verduras/provider/dark_theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
 
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
   const UserScreen({super.key});
 
   @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
   Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const ListTile(
-              title: Text(
-                'Address',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Subtitle here'),
-              leading: Icon(IconlyLight.profile),
-              trailing: Icon(IconlyLight.arrowRight2),
-            ),
             _listTiles(
                 title: 'Adress 2 ',
                 subtitle: 'My subtitle',
-                icon: IconlyBold.activity,
-                onPressed: () {})
+                icon: IconlyBold.profile,
+                onPressed: () {}),
+            _listTiles(
+              title: 'Orders',
+              icon: IconlyBold.bag,
+              onPressed: () {},
+            ),
+            _listTiles(
+              title: 'Wishlist',
+              icon: IconlyBold.heart,
+              onPressed: () {},
+            ),
+            _listTiles(
+              title: 'Viewed',
+              icon: IconlyBold.show,
+              onPressed: () {},
+            ),
+            _listTiles(
+              title: 'Forget password',
+              icon: IconlyBold.unlock,
+              onPressed: () {},
+            ),
+            SwitchListTile(
+              title: const Text('Theme'),
+              secondary: Icon(themeState.getDarkTheme
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined),
+              onChanged: (bool value) {
+                setState(() {
+                  themeState.setDarkTheme = value;
+                });
+              },
+              value: themeState.getDarkTheme,
+            ),
+            _listTiles(
+              title: 'Logout',
+              icon: IconlyBold.logout,
+              onPressed: () {},
+            ),
           ],
         ),
       ),
@@ -41,7 +77,7 @@ class UserScreen extends StatelessWidget {
         title,
         style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
-      subtitle: Text(subtitle!),
+      subtitle: Text(subtitle == null ? "" : subtitle),
       leading: Icon(icon),
       trailing: const Icon(IconlyLight.arrowRight2),
       onTap: () {
