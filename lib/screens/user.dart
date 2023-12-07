463a58f4889a447b64c16cb7967d2094d4884ec9
Lorena2387 <lorenaramirez23@hotmail.com>
2023-12-06 20:10:53 -0500

    implement text widget in user configuration

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,4 +1,5 @@
 import 'package:app_frutas_verduras/provider/dark_theme_provider.dart';
+import 'package:app_frutas_verduras/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:provider/provider.dart';
@@ -14,36 +15,40 @@ class _UserScreenState extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             _listTiles(
-                title: 'Adress 2 ',
-                subtitle: 'My subtitle',
-                icon: IconlyLight.profile,
-                onPressed: () {}),
+              title: 'Adress 2 ',
+              subtitle: 'My subtitle',
+              icon: IconlyLight.profile,
+              onPressed: () {},
+              color: color,
+            ),
             _listTiles(
               title: 'Orders',
               icon: IconlyLight.bag,
               onPressed: () {},
+              color: color,
             ),
             _listTiles(
-              title: 'Wishlist',
-              icon: IconlyLight.heart,
-              onPressed: () {},
-            ),
+                title: 'Wishlist',
+                icon: IconlyLight.heart,
+                onPressed: () {},
+                color: color),
             _listTiles(
-              title: 'Viewed',
-              icon: IconlyLight.show,
-              onPressed: () {},
-            ),
+                title: 'Viewed',
+                icon: IconlyLight.show,
+                onPressed: () {},
+                color: color),
             _listTiles(
-              title: 'Forget password',
-              icon: IconlyLight.unlock,
-              onPressed: () {},
-            ),
+                title: 'Forget password',
+                icon: IconlyLight.unlock,
+                onPressed: () {},
+                color: color),
             SwitchListTile(
               title: const Text('Theme'),
               secondary: Icon(themeState.getDarkTheme
@@ -57,27 +62,33 @@ class _UserScreenState extends State<UserScreen> {
               value: themeState.getDarkTheme,
             ),
             _listTiles(
-              title: 'Logout',
-              icon: IconlyLight.logout,
-              onPressed: () {},
-            ),
+                title: 'Logout',
+                icon: IconlyLight.logout,
+                onPressed: () {},
+                color: color),
           ],
         ),
       ),
     );
   }
 
-  Widget _listTiles(
-      {required String title,
-      String? subtitle,
-      required IconData icon,
-      required Function onPressed}) {
+  Widget _listTiles({
+    required String title,
+    String? subtitle,
+    required IconData icon,
+    required Function onPressed,
+    required Color color,
+  }) {
     return ListTile(
       title: Text(
         title,
         style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
-      subtitle: Text(subtitle == null ? "" : subtitle),
+      subtitle: TextWidget(
+        text: subtitle == null ? "" : subtitle,
+        color: color,
+        textSize: 18,
+      ),
       leading: Icon(icon),
       trailing: const Icon(IconlyLight.arrowRight2),
       onTap: () {
