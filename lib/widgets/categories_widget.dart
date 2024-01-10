816268a7289e90540eb789a8a31e07c757642b10
Lorena2387 <lorenaramirez23@hotmail.com>
2024-01-10 13:00:36 -0500

    implement the category desing

--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -1,11 +1,17 @@
+import 'package:app_frutas_verduras/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/dark_theme_provider.dart';
 
 class CategoriesWidget extends StatelessWidget {
   const CategoriesWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
     double _screenWidth = MediaQuery.of(context).size.width;
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
     return Container(
       decoration: BoxDecoration(
         color: Colors.red.withOpacity(0.1),
@@ -24,7 +30,8 @@ class CategoriesWidget extends StatelessWidget {
               image: DecorationImage(
                   image: AssetImage('asset/cat/veg.png'), fit: BoxFit.fill),
             ),
-          )
+          ),
+          TextWidget(text: 'Category name', color: color, textSize: 15)
         ],
       ),
     );
